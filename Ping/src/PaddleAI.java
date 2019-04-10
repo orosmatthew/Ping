6234535d2d6675db767feada864f0049df66bbb1
orosmatthew20 <orosmatthew@gmail.com>
2019-04-10 11:41:08 -0400

    Adjust Paddle AI

--- Ping/src/PaddleAI.java
+++ Ping/src/PaddleAI.java
@@ -1,20 +1,20 @@
 import java.awt.Color;
 
 public class PaddleAI extends Paddle {
-	
 	public PaddleAI(double x, double y, int sx, int sy, Color c, double s) {
 		super(x, y, sx, sy, c, s);
 	}
 	
 	public void process(double delta) {
 		currentSpeed = 0;
-		if (Ping.mainBall.getPosY()<(posY+(sizeY*0.5))) {
+		if (Ping.mainBall.getPosY()<(posY+(sizeY*0.5))&&Ping.mainBall.getPosX()>230) {
+
 			if(posY>=10) {
 				currentSpeed = speed;
 				posY-=(currentSpeed*delta);
 			}
 		}
-		if (Ping.mainBall.getPosY()>(posY+(sizeY*0.5))) {
+		if (Ping.mainBall.getPosY()>(posY+(sizeY*0.5))&&Ping.mainBall.getPosX()>230) {
 			if(posY<=411) {
 				currentSpeed = -speed;
 				posY-=(currentSpeed*delta);
